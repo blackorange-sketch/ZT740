9cc28fc911147811360dd871a7e2d6bde4eb60a3
blackorange-sketch <mtolikm@gmail.com>
2026-09-09 08:20:36 +0200

    Fix Meson Android cross file

--- build_turnip.sh
+++ build_turnip.sh
@@ -363,101 +363,54 @@ compile_mesa() {
     echo "$cver"
 
 
-    if [ "$use_ccache" = "1" ]; then
-
-
-        cat <<EOF > android-cross.txt
-
-[binaries]
-
+    
 ar = '$ndk_bin/llvm-ar'
 
-c = [
-    'ccache',
-    '$ndk_bin/aarch64-linux-android${cver}-clang',
-    '--sysroot=$ndk_sys'
-]
+if [ "$use_ccache" = "1" ]; then
 
-cpp = [
-    'ccache',
-    '$ndk_bin/aarch64-linux-android${cver}-clang++',
-    '--sysroot=$ndk_sys'
-]
-
-c_ld = 'lld'
-
-cpp_ld = 'lld'
-
-strip = '$ndk_bin/aarch64-linux-android-strip'
-
-
-[host_machine]
-
-system = 'android'
-
-cpu_family = 'aarch64'
-
-cpu = 'armv8'
-
-endian = 'little'
-
-
-[built-in options]
-
-c_link_args = ['-static-libstdc++']
-
-cpp_link_args = ['-static-libstdc++']
-
-EOF
-
-
-    else
-
-
-        cat <<EOF > android-cross.txt
-
+    cat > android-cross.txt <<EOF
 [binaries]
-
 ar = '$ndk_bin/llvm-ar'
-
-c = [
-    '$ndk_bin/aarch64-linux-android${cver}-clang',
-    '--sysroot=$ndk_sys'
-]
-
-cpp = [
-    '$ndk_bin/aarch64-linux-android${cver}-clang++',
-    '--sysroot=$ndk_sys'
-]
-
+c = ['ccache', '$ndk_bin/aarch64-linux-android${cver}-clang', '--sysroot=$ndk_sys']
+cpp = ['ccache', '$ndk_bin/aarch64-linux-android${cver}-clang++', '--sysroot=$ndk_sys']
 c_ld = 'lld'
-
 cpp_ld = 'lld'
-
 strip = '$ndk_bin/aarch64-linux-android-strip'
 
-
 [host_machine]
-
 system = 'android'
-
 cpu_family = 'aarch64'
-
 cpu = 'armv8'
-
 endian = 'little'
 
-
 [built-in options]
-
 c_link_args = ['-static-libstdc++']
-
 cpp_link_args = ['-static-libstdc++']
-
 EOF
 
+else
 
-    fi
+    cat > android-cross.txt <<EOF
+[binaries]
+ar = '$ndk_bin/llvm-ar'
+c = ['$ndk_bin/aarch64-linux-android${cver}-clang', '--sysroot=$ndk_sys']
+cpp = ['$ndk_bin/aarch64-linux-android${cver}-clang++', '--sysroot=$ndk_sys']
+c_ld = 'lld'
+cpp_ld = 'lld'
+strip = '$ndk_bin/aarch64-linux-android-strip'
+
+[host_machine]
+system = 'android'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+
+[built-in options]
+c_link_args = ['-static-libstdc++']
+cpp_link_args = ['-static-libstdc++']
+EOF
+
+fi
 
 
     echo
